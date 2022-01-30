44fcb6a83cfb9dbe7a31c07cccd6dfa632dca9c5
Angelo Fallaria <ba.fallaria@gmail.com>
2022-01-30 18:09:59 +0800

    update polybar

---
+++ polybar/scripts/mpd-song-scroll.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+
+import threading
+from time import sleep
+from sys import exit
+from mpd import MPDClient
+
+MAX_LEN = 32
+SLEEP_INTERVAL = 0.8
+
+
+# Scroll through a song's name left and right like a music player
+def scroll_song(name: str, max_len: int):
+    t = threading.current_thread()
+    i = 0
+
+    # Exit if max_len is greater than song length ( no need to scroll)
+    if max_len > len(name):
+        print(name)
+        return
+
+    while getattr(t, "do_run", True):
+        if i > len(name):
+            i = 1
+
+        output = name[i:max_len + i]
+
+        if i + max_len > len(name):
+            output += name[0:i]
+
+        output = output[:max_len]
+
+        i += 1
+        print(output, flush=True)
+        sleep(SLEEP_INTERVAL)
+
+
+client = MPDClient()
+client.connect("localhost", 6600)
+
+song_name = client.currentsong()['file']
+
+# if len(song_name) > MAX_LEN:
+#     scroll_song(song_name, MAX_LEN)
+# else:
+#     print(song_name)
+#     return 0
+
+# Open a thread to scroll through song name
+t = threading.Thread(target=scroll_song,
+                     args=(song_name, MAX_LEN))
+t.start()
+
+# Listen for player changes
+while True:
+    client.idle('player')
+
+    # Change the scrolling song when the mpd song changes
+    if client.currentsong()['file'] != song_name:
+        t.do_run = False
+
+        song_name = client.currentsong()['file']
+        t = threading.Thread(target=scroll_song,
+                             args=(song_name, MAX_LEN))
+        t.start()
