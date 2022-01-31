6799eb8415b5b40aca9fa5f7f885c6edabbcd798
Angelo Fallaria <ba.fallaria@gmail.com>
2022-02-01 05:46:17 +0800

    update stuff

--- scripts/play-song.sh
+++ scripts/play-song.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-song=$(mpc ls "$1" | dmenu_gruv -p "Play Music:")
+folder=$(mpc ls | dmenu_gruv "Select Album:")
+if [ $? != 0 ]
+then
+    exit
+fi
+
+if [ $1 = "album" ]
+then
+    mpc clear
+    mpc insert "$folder"
+    mpc play
+    exit 0
+fi
+
+song=$(mpc ls "$folder" | dmenu_gruv "Select Song:")
+if [ $? != 0 ]
+then
+    exit
+fi
+
 mpc clear
 mpc insert "$song"
 mpc play
